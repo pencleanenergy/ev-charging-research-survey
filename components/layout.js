12c5f327f534169aef6235f95b0611e8c2f77e7d
Aaron Bushnell <aabushnell@gmail.com>
2023-06-05 03:00:25 +0200

    disable header/footer

--- components/layout.js
+++ components/layout.js
@@ -4,9 +4,9 @@ import Footer from "./footer";
 export default function Layout({ children }) {
   return (
     <>
-      <Header />
+      {/* <Header /> */}
       <main>{children}</main>
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 }
