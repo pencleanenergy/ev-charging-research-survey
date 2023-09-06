f03956ceb8e3ac5419c22f880965bc489efcc5b1
James Bushnell <43382525+busbybushnell@users.noreply.github.com>
2023-09-06 10:57:46 -0700

    Tweak page 1.5

--- components/form_pages/page1_5.js
+++ components/form_pages/page1_5.js
@@ -16,13 +16,16 @@ export default function Page1_5({ setPage, monthly }) {
         pilot for a given level of incentive payment. If you are willing to
         participate for a payment that is less than or equal to our budgeted
         incentive payment, you will be enrolled in the program and receive our{" "}
-        {monthly ? "monthly" : "one-time"} incentive payment. If your survey
+        {monthly ? "monthly" : "one-time"} incentive payment. 
+      </p>  
+      <p className={styles.p_instructions}>
+        If your survey
         indicates that you would only participate for an incentive payment that
         is greater than our budgeted payment, you will not be enrolled in the
-        program and will not recieve any incentive payment. Again, your survey
-        answers will not influence the size of your payment. Your answers will
+        program and will not recieve any incentive payment. 
+        Again, your survey answers will not influence the size of your payment. Your answers will
         only determine whether you will be enrolled in our program.
-      </p>
+        </p>
       <div className={styles.buttonrow}>
         <button
           type="button"
